72ebc755ee96d61b1a905ded2d981117061536c4
Oleg Sigida <oleg.sigida@gmail.com>
2014-12-20 00:36:08 +0100

    sfsd

--- magic.py
+++ magic.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
-GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup((11,7), GPIO.OUT, initial=GPIO.LOW)
 GPIO.output(11, GPIO.HIGH)
 time.sleep(2)
 GPIO.output(11, GPIO.LOW)
