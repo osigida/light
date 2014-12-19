ee1646d37d2243a9da946f2a282245ba0e689261
Oleg Sigida <oleg.sigida@gmail.com>
2014-12-20 00:35:18 +0100

    dcss

--- magic.py
+++ magic.py
@@ -1,7 +1,9 @@
 import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
+GPIO.setwarnings(False)
+
+GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)
 GPIO.output(11, GPIO.HIGH)
 time.sleep(2)
 GPIO.output(11, GPIO.LOW)
