497bf7f41372fd53ae35fc8474e9948f0e678a5b
Oleg Sigida <oleg.sigida@gmail.com>
2014-12-19 23:52:23 +0100

    sdfsd

---
+++ light/magic.py
@@ -0,0 +1,29 @@
+__author__ = 'osigida'
+import time
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+
+GPIO.setmode(GPIO.BOARD)
+#GPIO.setup(channel, GPIO.OUT, initial=GPIO.LOW)
+
+
+chan_list = (11,7)
+GPIO.setup(11, GPIO.OUT, initial=False)
+GPIO.output(11, GPIO.LOW) # all LOW
+#GPIO.cleanup()
+
+# time.sleep(2)
+# GPIO.setup(12, GPIO.OUT)
+# GPIO.output(12, 1)
+# time.sleep(2)
+# GPIO.cleanup()
+# exit (0)
+# #GPIO.output(11, GPIO.HIGH) # all LOW
+# #GPIO.output(40, GPIO.HIGH)  # first LOW, second HIGH
+# time.sleep(2)
+# #exit (0)
+# #GPIO.output(38, GPIO.HIGH)  # first LOW, second HIGH
+# #GPIO.output(40, GPIO.LOW)
+# GPIO.cleanup()
